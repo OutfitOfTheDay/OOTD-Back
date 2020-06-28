8332fce28b3e19a6fbd583599c66e8955fe8344a
hagyeong <gkrud4513@naver.com>
2020-06-28 16:53:40 +0900

    [UPDATE] app.js

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const cors = require('cors');
 
 const app = express();
 
+const passportConfig = require('./config/passport');
 const api = require('./routes/api');
-const config = require('./config');
+const config = require('./config/config');
+
 const port = process.env.PORT;
+passportConfig(passport);
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(morgan('dev'));
 app.use('/static', express.static(__dirname + '/public'));
-app.set('jwt-secret', config.secret);
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/',api);
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect(config.mongodbUri,{ useNewUrlParser: true })
