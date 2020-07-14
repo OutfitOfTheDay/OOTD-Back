11d8cda08d6338e245f256b91b531bd30cc70e6f
hagyeong <gkrud4513@naver.com>
2020-07-14 15:55:48 +0900

    [UPDATE] update middleware

--- routes/api/mypage/index.js
+++ routes/api/mypage/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controller = require('./mypage.controller');
 const router = express.Router();
 const multer = require('multer');
-const ensureAuthenticated = require('../../../middleware/auth');
+const middleware = require('../../../middleware/auth');
 
 let _storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -15,9 +15,9 @@ let _storage = multer.diskStorage({
 
 let upload = multer({ storage: _storage });
 
-router.get('/',ensureAuthenticated,controller.seeMypage);
-router.patch('/',ensureAuthenticated,upload.single('profile'),controller.updateMypage);
-router.get('/myfeed',ensureAuthenticated,controller.seeMyfeed);
-router.get('/tagfeed',ensureAuthenticated,controller.seeTagfeed);
+router.get('/',middleware,controller.seeMypage);
+router.patch('/',middleware,upload.single('profile'),controller.updateMypage);
+router.get('/myfeed',middleware,controller.seeMyfeed);
+router.get('/tagfeed',middleware,controller.seeTagfeed);
 
 module.exports = router;
--- routes/api/post/index.js
+++ routes/api/post/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('./post.controller');
 const router = express.Router();
 const multer = require('multer');
+const middleware = require('../../../middleware/auth');
 
 let _storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -13,11 +14,11 @@ let _storage = multer.diskStorage({
 });
 let upload = multer({ storage: _storage });
 
-router.post('/post',upload.array('pictures'),controller.writePost);
-router.get('/post/:postId',controller.seePost);
-router.post('/comment',controller.writeComment);
-router.patch('/post/:postId',upload.array('pictures'),controller.updatePost);
-router.delete('/post/:postId',controller.deletePost);
-router.get('/like/:postId',controller.likePost);
+router.post('/post',middleware,upload.array('pictures'),controller.writePost);
+router.get('/post/:postId',middleware,controller.seePost);
+router.post('/comment',middleware,controller.writeComment);
+router.patch('/post/:postId',middleware,upload.array('pictures'),controller.updatePost);
+router.delete('/post/:postId',middleware,controller.deletePost);
+router.get('/like/:postId',middleware,controller.likePost);
 
 module.exports = router;
