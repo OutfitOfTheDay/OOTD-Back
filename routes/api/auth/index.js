b911b0e59dd217cd7a0e2401131bd63b6bb1ff23
hagyeong <gkrud4513@naver.com>
2020-07-27 10:08:24 +0900

    [UPDATE] update auth/index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -4,26 +4,6 @@ const controller = require('./user.controller');
 const middleware = require('../../../middleware/auth');
 const router = express.Router();
 
-// router.get('/auth/facebook',passport.authenticate('facebook'));
-
-// router.get('/auth/facebook/callback',
-//   passport.authenticate('facebook',{ successRedirect:'/login_success',
-//     failureRedirect:'/login_fail'})
-// );
-
-// router.get('/auth/google', passport.authenticate('google',{scope:['https://www.googleapis.com/auth/plus.login']}));
-
-// router.get('/auth/google/callback',
-//   passport.authenticate('google',{ successRedirect:'/login_success',
-//     failureRedirect:'/login_fail'})
-// );
-
-// router.get('/login_success',ensureAuthenticated,controller.login_success);
-
-// router.get('/login_fail',controller.login_fail);
-    
-// router.get('/logout',controller.logout);
-
 router.post('/login', controller.login);
 
 module.exports = router;
