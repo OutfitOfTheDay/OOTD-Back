b47d8846366936b42e1c91a7c9d7ffeebbd17966
hagyeong <gkrud4513@naver.com>
2020-07-27 10:00:11 +0900

    [UPDATE] update mypage.controller.js

--- routes/api/mypage/mypage.controller.js
+++ routes/api/mypage/mypage.controller.js
@@ -6,8 +6,7 @@ const config = require('../../../config/config');
 const seeMypage = (req,res)=>{
   let userId = req.decoded.id
   const findUser = async(userId)=>{
-    let user =  await User.findOne({id:userId},{userName:1,profile:1})
-    .catch(e=>res.status(500).json(e));
+    let user =  await User.findOne({id:userId},{userName:1,profile:1});
     
     return user;
   }
@@ -28,11 +27,10 @@ const seeMypage = (req,res)=>{
 
 const updateMypage = (req,res)=>{
   const userId = req.decoded.id;
-  
   const updateMyInfo = async(userId)=>{
-    let user = await User.updateOne({id:userId},{userName: req.body.userName,profile:config.picturesUrl+req.file.filename})
-    .catch(e=>res.status(500).json(e));
-    
+    let user;
+    if(!req.file) user = await User.updateOne({id:userId},{userName: req.body.userName,profile:''})
+    else user = await User.updateOne({id:userId},{userName: req.body.userName,profile:config.picturesUrl+req.file.filename}).catch(e=>res.status(500).json(e));
     return user;
   }
 
@@ -53,10 +51,8 @@ const updateMypage = (req,res)=>{
 const seeMyfeed = (req,res)=>{
   const userId = req.decoded.id
   const findMyfeed = async(userId)=>{    
-    const posts = await Post.find({userId: userId})
-    .catch(e=>res.status(500).json(e));
-    const user = await User.findOne({id:userId})
-    .catch(e=>res.status(500).json(e));
+    const posts = await Post.find({userId: userId}).sort({date:-1}).sort({time:-1});
+    const user = await User.findOne({id:userId});
     let myfeed=[];
     posts.map(post=> myfeed.push({post,user}));
     return myfeed
@@ -77,22 +73,20 @@ const seeMyfeed = (req,res)=>{
 
 const seeTagfeed = (req,res)=>{
   const userId = req.decoded.id;
-
   const findMyInfo = async(userId)=>{
-    const user = await User.findById({id:userId})
-    .catch(e=>res.status(500).json(e));
+    const user = await User.findOne({id:userId});
     return user;
   }
-  const findTagfeed = async(user)=>{
+  const findTagfeed = async(users)=>{
     let tagfeed=[];
-    for(let i=0;i<user.likedId.length;i++){
-      let post = await Post.findOne({_id: user.likedId[i]})
-      .catch(e=>res.status(500).json(e));
-      let users = await User.findOne({id:post.userId})
-      .catch(e=>res.status(500).json(e));
-      tagfeed.push({post,users});
+    let posts = await Post.find().sort({date:-1}).sort({time:-1});
+    for(let i=0;i<posts.length;i++){
+      if(users.likedId.length==tagfeed.length) break;
+      if(users.likedId.includes(posts[i]._id)){
+        let user = await User.findOne({id:posts[i].userId})
+        tagfeed.push({post:posts[i],user})
+      }
     }
-    
     return tagfeed;
   }
   const response = (tagfeed)=>{
@@ -104,6 +98,7 @@ const seeTagfeed = (req,res)=>{
       err: err.message
     });
   }
+
   findMyInfo(userId)
   .then(findTagfeed)
   .then(response)
