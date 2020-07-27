a5b4e97f2a71bcde5f1205b7ca230cceea0bf60e
hagyeong <gkrud4513@naver.com>
2020-07-27 10:08:48 +0900

    [UPDATE] update auth/index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const passport = require('passport');
 const controller = require('./user.controller');
-const middleware = require('../../../middleware/auth');
 const router = express.Router();
 
 router.post('/login', controller.login);
